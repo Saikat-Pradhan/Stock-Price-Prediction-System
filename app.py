98a2b603a59e76141452bba0585aeee9899be9a0
Saikat Pradhan <saikatpradhan65@gmail.com>
2026-02-24 23:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ warnings.filterwarnings('ignore')
 # Load the model
 model = pkl.load(open('model.pkl', 'rb'))
 
-# Define the stock ticker and date range
+# Define the default stock ticker and date range
 start = '2010-01-01'
 end = '2019-12-31'
 
@@ -19,108 +19,123 @@ end = '2019-12-31'
 st.title('Stock Price Prediction App')
 
 # Get user input for stock ticker
-input = st.selectbox('Select Your Company', ['Apple', 'State Bank of India', 'Google', 'Microsoft', 'Amazon', 'Tesla'])
-if input == 'Apple':
-    ticker = 'AAPL'
-elif input == 'State Bank of India':
-    ticker = 'SBIN.NS'
-elif input == 'Google':
-    ticker = 'GOOGL'
-elif input == 'Microsoft':
-    ticker = 'MSFT'
-elif input == 'Amazon':
-    ticker = 'AMZN'
-elif input == 'Tesla':
-    ticker = 'TSLA'
+companies = ['Apple', 'State Bank of India', 'Google', 'Microsoft', 'Amazon', 'Tesla']
 
-# Fetch stock data
-if ticker:
-    df = yf.download(ticker, start=start, end=end)
-    df = df.reset_index()
-    df.columns = df.columns.droplevel(1)
-    df = df[['Date', 'High', 'Low', 'Open', 'Close', 'Volume']]
+input = st.selectbox(
+    'Select Your Company',
+    ['-- Select here --'] + companies,
+    index=0
+)
 
-    # Describe Data
-    st.subheader(f"{input} Stock Price Data (2010-2019)")
-    st.write(df.describe())
-    
-    # Visualizations
-    st.subheader(f"{input} Closing Price vs Time Chart")
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot(df['Date'], df['Close'], label='Close')
-    plt.legend()
-    plt.grid(True)
-    st.pyplot(fig)
+if input != '-- Select here --':
+    if input == 'Apple':
+        ticker = 'AAPL'
+    elif input == 'State Bank of India':
+        ticker = 'SBIN.NS'
+    elif input == 'Google':
+        ticker = 'GOOGL'
+    elif input == 'Microsoft':
+        ticker = 'MSFT'
+    elif input == 'Amazon':
+        ticker = 'AMZN'
+    elif input == 'Tesla':
+        ticker = 'TSLA'
 
-    # Closing Price chart with 100MA
-    st.subheader(f"{input} Closing Price vs Time Chart with 100MA")
-    ma100 = df['Close'].rolling(100).mean()
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot(df['Date'], df['Close'], label='Close')
-    plt.plot(df['Date'], ma100, 'r', label='100MA')
-    plt.legend()
-    plt.grid(True)
-    st.pyplot(fig)
+    # Taking input of start and end date
+    start = st.date_input('Start Date', value=pd.to_datetime(start))
+    end = st.date_input('End Date', value=pd.to_datetime(end))
 
-    # Closing Price chart with 100MA and 200MA
-    st.subheader(f"{input} Closing Price vs Time Chart with 200MA")
-    ma200 = df['Close'].rolling(200).mean()
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot(df['Date'], df['Close'], label='Close')
-    plt.plot(df['Date'], ma100, 'r', label='100MA')
-    plt.plot(df['Date'], ma200, 'g', label='200MA')
-    plt.legend()
-    plt.grid(True)
-    st.pyplot(fig)
+    # Fetch stock data
+    if ticker:
+        df = yf.download(ticker, start=start, end=end)
+        df = df.reset_index()
+        df.columns = df.columns.droplevel(1)
+        df = df[['Date', 'High', 'Low', 'Open', 'Close', 'Volume']]
 
-    # Splitting data into Training and Testing
-    data_training = pd.DataFrame(df['Close'][0:int(len(df) * 0.70)])
-    data_testing = pd.DataFrame(df['Close'][int(len(df) * 0.70):int(len(df))])
+        # Describe Data
+        st.subheader(f"{input} Stock Price Data ({start} to {end})")
+        st.write(df.describe())
 
-    # Scaling
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler(feature_range=(0, 1))
+        # Visualizations
+        st.subheader(f"{input} Closing Price vs Time Chart")
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(df['Date'], df['Close'], label='Close')
+        plt.legend()
+        plt.grid(True)
+        st.pyplot(fig)
 
-    data_training_array = scaler.fit_transform(data_training)
+        # Closing Price chart with 100MA
+        st.subheader(f"{input} Closing Price vs Time Chart with 100MA")
+        ma100 = df['Close'].rolling(100).mean()
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(df['Date'], df['Close'], label='Close')
+        plt.plot(df['Date'], ma100, 'r', label='100MA')
+        plt.legend()
+        plt.grid(True)
+        st.pyplot(fig)
 
-    # Splitting data into x_train and y_train
-    x_train = []
-    y_train = []
+        # Closing Price chart with 100MA and 200MA
+        st.subheader(f"{input} Closing Price vs Time Chart with 200MA")
+        ma200 = df['Close'].rolling(200).mean()
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(df['Date'], df['Close'], label='Close')
+        plt.plot(df['Date'], ma100, 'r', label='100MA')
+        plt.plot(df['Date'], ma200, 'g', label='200MA')
+        plt.legend()
+        plt.grid(True)
+        st.pyplot(fig)
 
-    for i in range(100, data_training_array.shape[0]):
-        x_train.append(data_training_array[i - 100:i])
-        y_train.append(data_training_array[i, 0])
+        # Splitting data into Training and Testing
+        data_training = pd.DataFrame(df['Close'][0:int(len(df) * 0.70)])
+        data_testing = pd.DataFrame(df['Close'][int(len(df) * 0.70):int(len(df))])
 
-    x_train, y_train = np.array(x_train), np.array(y_train)
+        # Scaling
+        from sklearn.preprocessing import MinMaxScaler
 
-    # Testing part
-    past_100_days = data_training.tail(100)
-    final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
-    input_data = scaler.transform(final_df)
+        scaler = MinMaxScaler(feature_range=(0, 1))
 
-    x_test = []
-    y_test = []
+        data_training_array = scaler.fit_transform(data_training)
 
-    for i in range(100, input_data.shape[0]):
-        x_test.append(input_data[i - 100:i])
-        y_test.append(input_data[i, 0])
+        # Splitting data into x_train and y_train
+        x_train = []
+        y_train = []
 
-    x_test, y_test = np.array(x_test), np.array(y_test)
-    y_pred = model.predict(x_test)
+        for i in range(100, data_training_array.shape[0]):
+            x_train.append(data_training_array[i - 100:i])
+            y_train.append(data_training_array[i, 0])
 
-    scaler = scaler.scale_
-    scale_factor = 1/scaler[0]
-    y_pred = y_pred * scale_factor
-    y_test = y_test * scale_factor
+        x_train, y_train = np.array(x_train), np.array(y_train)
 
-    # Final Graph
-    st.subheader(f"{input} Predicted vs Actual Closing Price Data")
+        # Testing part
+        past_100_days = data_training.tail(100)
+        final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+        input_data = scaler.transform(final_df)
 
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot(y_test, 'b', label='Original Price')
-    plt.plot(y_pred, 'r', label='Predicted Price')
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.legend()
-    plt.grid(True)
-    st.pyplot(fig)
+        x_test = []
+        y_test = []
+
+        for i in range(100, input_data.shape[0]):
+            x_test.append(input_data[i - 100:i])
+            y_test.append(input_data[i, 0])
+
+        x_test, y_test = np.array(x_test), np.array(y_test)
+        y_pred = model.predict(x_test)
+
+        scaler = scaler.scale_
+        scale_factor = 1 / scaler[0]
+        y_pred = y_pred * scale_factor
+        y_test = y_test * scale_factor
+
+        # Final Graph
+        st.subheader(f"{input} Predicted vs Actual Closing Price Data")
+
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(y_test, 'b', label='Original Price')
+        plt.plot(y_pred, 'r', label='Predicted Price')
+        plt.xlabel('Time')
+        plt.ylabel('Price')
+        plt.legend()
+        plt.grid(True)
+        st.pyplot(fig)
+else:
+    st.info("Please select a company to continue.")
